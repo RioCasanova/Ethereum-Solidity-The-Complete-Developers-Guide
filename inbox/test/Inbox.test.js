b8d28eac4e33fccd7746fefa6b8d20af16d1021f
Rio Casanova <riocasanova@gmail.com>
2023-10-01 20:26:55 -0600

    first smart contract

---
+++ inbox/test/Inbox.test.js
@@ -0,0 +1,38 @@
+const ganache = require("ganache");
+const { Web3 } = require("web3");
+const assert = require("assert");
+const web3 = new Web3(ganache.provider());
+const { interface, bytecode } = require("../compile");
+// updated ganache and web3 imports added for convenience
+
+// contract test code will go here
+let accounts;
+let inbox;
+
+beforeEach(async () => {
+  // Get a list of all accounts
+  accounts = await web3.eth.getAccounts();
+
+  // Use one of those accounts to deploy the contract
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({
+      data: bytecode,
+      arguments: ["Hi there!"],
+    })
+    .send({ from: accounts[0], gas: "1000000" });
+});
+
+describe("Inbox", () => {
+  it("Can Deploy", () => {
+    assert.ok(inbox.options.address);
+  });
+  it("has a default message", async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal(message, "Hi there!");
+  });
+  it("can change the message", async () => {
+    await inbox.methods.setMessage("bye").send({ from: accounts[0] });
+    const message = await inbox.methods.message().call();
+    assert.equal(message, "bye");
+  });
+});
